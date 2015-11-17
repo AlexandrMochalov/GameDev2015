5d9de5dd0fcbbb3ab3b716cf21f1b328d49f105d
AlexandrMochalov <oleksandr.mochalov@gmail.com>
2015-11-17 08:26:22 +0200

    Anim

---
+++ po/Assets/Scripts/KeyListener.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyListener : MonoBehaviour {
+	private Animator _animator;
+	private float _speed = 0.0f; 
+	private bool _called = false;
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool space = false;
+
+		if (Input.GetMouseButton(0)) space = true;
+
+		_speed += Time.deltaTime;	
+		if (!enabled) 
+		{
+
+		}
+		_animator.SetBool("Space", space);
+	}
+
+	void OnLook () {
+		//GetComponent<AudioSource>().Play();
+	}
+}
---
+++ po/Assets/Scripts/MySecondsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MySecondsScript : MonoBehaviour {
+	[HideInInspector]
+	public string MyGameName = "Game";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		GetComponent<SpriteRenderer>().color = Color.red;
+		GetComponent<Transform>().position = new Vector3(0f,0f,0f);
+		transform.position = new Vector3(0f,0f,0f);
+	}
+}
