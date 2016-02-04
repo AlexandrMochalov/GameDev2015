250dc00671c376e07f515541acbcb0327d0df7dc
AlexandrMochalov <oleksandr.mochalov@gmail.com>
2016-02-04 17:26:42 +0200

    Cards & dragdrop

---
+++ po/Assets/Drag.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Drag : MonoBehaviour {
+    public enum CardType
+    {
+        Heatrs,
+        Diamonds,
+        Clovers,
+        Pikes
+    }
+
+    public enum CardColor {
+        Red,
+        Black
+    }
+    
+    public Vector3 Offset = Vector3.zero;
+    public CardType Type = CardType.Heatrs;
+    public int Value;
+    public CardColor ColorType = CardColor.Red;
+
+    bool isMouseDown = false;
+    bool isDrag = false;
+    Vector3 startPosition;
+    string sortindLayerName;
+
+    List<Slot> slots = new List<Slot> ();
+    Slot currentSlot;
+
+    void Start() {
+        startPosition = transform.position;
+        ColorType = (Type == CardType.Diamonds || Type == CardType.Heatrs) ? CardColor.Red : CardColor.Black;
+        sortindLayerName = GetComponent<SpriteRenderer>().sortingLayerName;
+    }
+
+    void OnMouseDown() {
+        isMouseDown = true;
+        GetComponent<SpriteRenderer>().sortingLayerName = "Drag";
+    }
+
+    void OnMouseUp() {
+       
+        if (!isDrag) return;
+        isMouseDown = false;
+        isDrag = false;
+
+        if (slots.Count == 0)
+        {
+            iTween.MoveTo(gameObject
+                , iTween.Hash("position", startPosition, "time", 0.5f));
+
+            Invoke("OnCancalDragMoveEnd", 0.5f);
+        }
+        else
+        {
+            Slot nearest = slots[0];
+            float distance = GetDistanceForSlot(nearest);
+            foreach (var slot in slots)
+            {
+                var newDistance = GetDistanceForSlot(slot);
+                if (newDistance < distance) {
+                    nearest = slot;
+                    distance = newDistance;
+                }
+            }
+            nearest.OnDrop(this);
+            currentSlot = nearest;
+
+            var pos = nearest.transform.position;
+            iTween.MoveTo(gameObject
+                , iTween.Hash("position", new Vector3 (pos.x, pos.y, transform.position.z)));
+            GetComponent<SpriteRenderer>().sortingLayerName = sortindLayerName;
+        }
+    }
+
+    void OnCancalDragMoveEnd() {
+        GetComponent<SpriteRenderer>().sortingLayerName = sortindLayerName;
+        GetComponent<SpriteRenderer>().sortingOrder = 1;
+    }
+
+    float GetDistanceForSlot (Slot slot)
+    {
+        var ourCenter = GetComponent<Collider2D>().bounds.center;
+        var otherCenter = slot.GetComponent<Collider2D>().bounds.center;
+        return Mathf.Abs((ourCenter - otherCenter).magnitude);
+    }
+
+    void OnMouseDrag() {
+        if (!isMouseDown) return;
+        if (!isDrag) {
+            if (currentSlot != null)
+                currentSlot.RemoveCard(this);
+            currentSlot = null;
+            isDrag = true;
+        }
+
+        var mousePos = Input.mousePosition;
+        var worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        
+        transform.position = Offset + new Vector3 (worldPosition.x
+            , worldPosition.y
+            , transform.position.z);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        var slot = other.GetComponent<Slot>();
+        if (slot != null && slot.CanReceiveDrop(this))
+            slots.Add(slot);
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        var slot = other.GetComponent<Slot>();
+        if (slot != null && slots.Contains(slot))
+            slots.Remove(slot);
+    }
+}
---
+++ po/Assets/Slot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour {
+    List<Drag> stack = new List<Drag>();
+
+    bool IsTypeCopability(Drag.CardColor first, Drag.CardColor second)
+    {
+        return first != second;
+    }
+
+    public bool CanReceiveDrop(Drag dragObject)
+    {
+        return (stack.Count == 0)
+            || (IsTypeCopability(stack[stack.Count-1].ColorType, dragObject.ColorType));
+    }
+    
+    public void OnDrop(Drag dragObject) {
+        var pos = dragObject.transform.position;
+        if (stack.Count > 0)
+            stack[stack.Count - 1].GetComponent<Collider2D>().enabled = false;
+
+        stack.Add(dragObject);
+        
+        dragObject.GetComponent<SpriteRenderer>().sortingOrder = stack.Count;
+        Debug.LogError("Add " + stack.Count);
+    }
+
+    public void RemoveCard(Drag dragObject)
+    {
+        stack.Remove(dragObject);
+        if (stack.Count > 0)
+            stack[stack.Count - 1].GetComponent<Collider2D>().enabled = true;
+        Debug.LogError("Remove " + stack.Count);
+    }
+}
